70334dc9c48afa49622cf86dfb4ba4d9f2225204
ValeryVasilevich <>
2024-10-25 15:49:04 +0200

    Impl: exercise 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,24 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger count = 0;
+    NSInteger n = ratingArray.count;
+
+    for (NSInteger i = 0; i < n - 2; i++) {
+        for (NSInteger j = i + 1; j < n - 1; j++) {
+            for (NSInteger k = j + 1; k < n; k++) {
+                NSInteger ratingI = [ratingArray[i] integerValue];
+                NSInteger ratingJ = [ratingArray[j] integerValue];
+                NSInteger ratingK = [ratingArray[k] integerValue];
+
+                if ((ratingI < ratingJ && ratingJ < ratingK) || (ratingI > ratingJ && ratingJ > ratingK)) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
 }
 
 @end
